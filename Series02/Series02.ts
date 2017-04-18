8102c77aaef59f804a9e655294d654011a50cbd9
Christoph Mende <mende.christoph@gmail.com>
2017-04-18 16:17:28 +0200

    Add Series02.ts

---
+++ Series02/Series02.ts
@@ -0,0 +1,30 @@
+function series02() {
+  // TypeScript-Variablendeklaration: Wert kann geändert werden
+  let a = 'a'
+  a = 'a2'
+  // Fehler: Variable kann nicht erneut deklariert werden
+  //let a = 'a3'
+  if (true) {
+    let a3 = 'a3'
+  }
+  // Fehler: Variable außerhalb ihrer Scope nicht verfügbar
+  //alert(a3)
+
+  // Konstantendeklaration: Wert kann nicht geändert werden
+  // ansonsten gleiche Einschränkungen wie let
+  const b = 'b'
+  // Fehler:
+  //b = 'b2'
+
+  // JavaScript-Variablendeklaration: Wert kann geändert werden
+  // im Gegensatz zu let ist diese Deklaration nicht nur in ihrer Scope sichtbar
+  // wie das folgende Beispiel zeigt
+  if (true) {
+    var c = 'c'
+  }
+  alert(c)
+  // zusätzlich kann die Variable 'shadowed' - also erneut deklariert - werden,
+  // auch das ist mit let nicht möglich
+  var c = 'c2'
+  var c = 'c3'
+}

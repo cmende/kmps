31f416b41f575c7a73a5684b0c7f1d9f0387da5d
Christoph Mende <mende.christoph@gmail.com>
2017-04-22 16:55:49 +0200

    Remove unneeded semicolons

--- Series02/Series02.swift
+++ Series02/Series02.swift
@@ -1,10 +1,10 @@
-let arr : [Int] = [];
+let arr : [Int] = []
 
 // Swift:: Error: cannot use mutating member on immutable value: 'arr' is a 'let' constant
-// arr.append(1);
+// arr.append(1)
 // ^~~
 // tmp/GmralRMU3M.swift:2:1: note: change 'let' to 'var' to make it mutable
-// let arr : [Int] = [];
+// let arr : [Int] = []
 // ^~~
 // var
-arr.append(1);
+arr.append(1)
